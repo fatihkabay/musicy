ed54e052986f73ab67af6b65876fabad3c176c79
Fatih Kabay <mfthkabay@gmail.com>
2024-01-01 22:14:16 +0300

    access token and authorize success

--- src/components/Musicy.jsx
+++ src/components/Musicy.jsx
@@ -11,27 +11,107 @@ import {
 } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
+var redirect_uri = "http://localhost:3000/musicy";
+
+var client_id = "";
+var client_secret = "";
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
+
+function onPageLoad() {
+   if(window.location.search.length > 0) {
+     handleRedirect();
+   }
+}
+
+function handleRedirect() {
+  let code = getCode();
+  fetchAccessToken(code);
+}
+
+function fetchAccessToken(code) {
+  let body = "grant_type=authorization_code";
+  body += "&code=" + code;
+  body += "&redirect_uri=" + encodeURI(redirect_uri);
+  body += "&client_id=" + client_id;
+  body += "&client_secret" + client_secret;
+  callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body) {
+   let xhr = new XMLHttpRequest();
+   xhr.open("POST", TOKEN, true);
+   xhr.setRequestHeader('Content_Type', 'application/x-www-form-urlencoded');
+   xhr.setRequestHeader('Authorization', 'Basic', + btoa(client_id + ":" + client_secret));
+   xhr.send(body);
+   xhr.onload = handleAuthorizationResponse;
+}
+
+function getCode() {
+   let code = null;
+   const queryString = window.location.search;
+   if(queryString.length > 0) {
+     const urlParams = new URLSearchParams(queryString);
+     code = urlParams.get('code');
+   }
+   return code;
+}
+
+function requestAuthorization(){
+  client_id = document.getElementById("clientId").value;
+  client_secret = document.getElementById("clientSecret").value;
+  localStorage.setItem("client_id", client_id);
+  localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
+
+  let url = AUTHORIZE;
+  url += "?client_id=" + client_id;
+  url += "&response_type=code";
+  url += "&redirect_uri=" + encodeURI(redirect_uri);
+  url += "&show_dialog=true";
+  url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+  window.location.href = url; // Show Spotify's authorization screen
+}
+
 function Musicy() {
   const [searchInput, setSearchInput] = useState("");
 
+  
   return (
-    <Container>
-      <InputGroup className="mb-3" size="lg">
-        <FormControl
-          placeholder="Search For Artists"
-          type="input"
-          onKeyPress={(event) => {
-            if (event.key === "Enter") {
-              console.log("Pressed Enter");
-            }
-          }}
-          onChange={event => setSearchInput(event.target.value)}
-        />
-        <Button onClick={() => {console.log("clicked button")}}>
-          Search
-        </Button>
-      </InputGroup>
-    </Container>
+    <div className="app" onLoad={onPageLoad()}>
+      <Container id="tokenSection">
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Client ID"
+            type="input"
+            id="clientId"
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <FormControl
+            placeholder="Client Secret"
+            type="input"
+            id="clientSecret"
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <Button
+            onClick={() => {
+              requestAuthorization();
+            }}
+          >
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          <Card>
+            <Card.Img src="#" />
+            <Card.Body>
+              <Card.Title>Album Name Here</Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    </div>
   );
 }
 

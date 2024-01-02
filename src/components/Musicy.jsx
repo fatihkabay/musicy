042d71cdb083116107ede493e0eaac42e72d2b13
Fatih Kabay <mfthkabay@gmail.com>
2024-01-02 21:41:42 +0300

    handleAuth

--- src/components/Musicy.jsx
+++ src/components/Musicy.jsx
@@ -16,48 +16,73 @@ var redirect_uri = "http://localhost:3000/musicy";
 var client_id = "";
 var client_secret = "";
 
+var access_token = null;
+var refresh_token = null;
+
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
+const TOKEN = "https://accounts.spotify.com/api/token";
 
 function onPageLoad() {
-   if(window.location.search.length > 0) {
-     handleRedirect();
-   }
+  localStorage.getItem("client_id", client_id);
+  localStorage.getItem("client_secret", client_secret);
+  if (window.location.search.length > 0) {
+    handleRedirect();
+  }
 }
 
 function handleRedirect() {
   let code = getCode();
   fetchAccessToken(code);
+  window.history.pushState("", "", redirect_uri);
 }
 
 function fetchAccessToken(code) {
-  let body = "grant_type=authorization_code";
-  body += "&code=" + code;
-  body += "&redirect_uri=" + encodeURI(redirect_uri);
-  body += "&client_id=" + client_id;
-  body += "&client_secret" + client_secret;
-  callAuthorizationApi(body);
+ let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+function callAuthorizationApi(body){
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", TOKEN, true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+  xhr.send(body);
+  xhr.onload = handleAuthorizationResponse;
 }
 
-function callAuthorizationApi(body) {
-   let xhr = new XMLHttpRequest();
-   xhr.open("POST", TOKEN, true);
-   xhr.setRequestHeader('Content_Type', 'application/x-www-form-urlencoded');
-   xhr.setRequestHeader('Authorization', 'Basic', + btoa(client_id + ":" + client_secret));
-   xhr.send(body);
-   xhr.onload = handleAuthorizationResponse;
+function handleAuthorizationResponse() {
+  if (this.status === 200) {
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+    if (data.access_token !== undefined) {
+      access_token = data.access_token;
+      localStorage.setItem("access_token", access_token);
+    }
+    if (data.refresh_token !== undefined) {
+      refresh_token = data.refresh_token;
+      localStorage.setItem("refresh_token", refresh_token);
+    }
+    onPageLoad();
+  } else {
+    console.log(this.responseText);
+    alert(this.responseText);
+  }
 }
 
 function getCode() {
-   let code = null;
-   const queryString = window.location.search;
-   if(queryString.length > 0) {
-     const urlParams = new URLSearchParams(queryString);
-     code = urlParams.get('code');
-   }
-   return code;
+  let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
 }
 
-function requestAuthorization(){
+function requestAuthorization() {
   client_id = document.getElementById("clientId").value;
   client_secret = document.getElementById("clientSecret").value;
   localStorage.setItem("client_id", client_id);
@@ -68,14 +93,14 @@ function requestAuthorization(){
   url += "&response_type=code";
   url += "&redirect_uri=" + encodeURI(redirect_uri);
   url += "&show_dialog=true";
-  url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+  url +=
+    "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
   window.location.href = url; // Show Spotify's authorization screen
 }
 
 function Musicy() {
   const [searchInput, setSearchInput] = useState("");
 
-  
   return (
     <div className="app" onLoad={onPageLoad()}>
       <Container id="tokenSection">
